349f9b819ea6666e6197e92587fc9ce31e4a2ba1
Tai-Lung1 <thebadsideofme12@gmail.com>
2024-07-22 07:32:23 +0530

    why am i doing this

---
+++ QN1.java
@@ -0,0 +1,10 @@
+public class QN1 {
+    public static void main(String args){
+        int n=8;
+        for(int i=0;i<n;i++)
+            for(int j=n-1;j>0;j--){
+
+            }
+        System.out.println();
+    }
+}
